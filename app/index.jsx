389992f3aa9384e1cc2c3e3023b5bd340716e940
ntthuyvy73 <ntthuyvy73@gmail.com>
2024-04-23 17:01:53 +0700

    commit

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-4xl font-pblack ">Test 7</Text>
+            <StatusBar style="auto" />
+            <Link href="/home">Home</Link>
+        </View>
+    );
+}
